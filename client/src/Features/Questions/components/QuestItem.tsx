f84def4bc37edcc46b6925af6434f37e5b4bff7a
Dianaanaas <nasibullina.di@yandex.ru>
2024-02-23 14:05:30 +0300

    done

--- client/src/Features/Questions/components/QuestItem.tsx
+++ client/src/Features/Questions/components/QuestItem.tsx
@@ -1,19 +1,58 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import type { Quest } from '../type'
 import '../Questions.css'
+import * as api from '../api'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../../../store/store'
+import * as apiApp from '../../../App/api'
+import { useNavigate } from 'react-router'
 
 function QuestItem({question}:{question:Quest}):JSX.Element {
   const [modals, setModals] = useState(false)
   const [disabled, setDisabled] = useState(false)
 
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  
+  const [message, setMessage] = useState('')
 
+  useEffect(()=>{
+    apiApp.getUser()
+    .then(data=>{
+     if(data.message === 'ok'){
+       dispatch({type:'auth/registration',payload:data})
+       
+       if (!data.user) {
+         navigate('/')
+       }
+     } 
+    })
+    .catch(console.log)   
+   },[message])
+  // const answers = useSelector((store:RootState)=>store.qustionsState.themes.)
+
+ const choiceAnswer=(e:React.FormEvent<HTMLFormElement> )=>{
+  e.preventDefault()
+api.chekedAnswerFetch({id:question.id, idAnswer:e.target.answer.value}).then((data)=> {
+  console.log(data);
+  if(data.message === 'ok'){
+    setMessage('Ответ верный!')
+  }else{
+    setMessage(`${data.message} Правильный ответ: ${data.rightAnswer.answer_text}`)
+  }
+  console.log(message);
+  
+}
+)
+.catch(console.log)
+  }
 
   return (
     <>
     <button type='button' className={disabled ? 'disabled' : ''} onClick={
       ()=> setModals(true)
     } >{
-        question.id
+        question.salary
         }
     </button>
 {
@@ -25,6 +64,15 @@ function QuestItem({question}:{question:Quest}):JSX.Element {
       setDisabled(true)
       }} >x</button>
     <h2>{question.question_text}</h2>
+    <form onSubmit={choiceAnswer}>
+      {question.Answers.map((el)=>
+      <>
+      <input  id={el.id} type="radio" name='answer' value={el.id}/>
+      <label htmlFor={el.id}>{el.answer_text}</label> </>)
+      }
+      <button type='submit'>yrtrfh</button>
+      <div className='answerErr'>{message}</div>
+    </form>
   </div>
   </div>
 }
--- client/src/Features/Questions/type.ts
+++ client/src/Features/Questions/type.ts
@@ -1,9 +1,20 @@
+export type Answer = {
+    id:number,
+    answer_text:string,
+    question_id:number,
+    isRight:boolean
+}
 export type Quest = {
     id:number;
     question_text:string;
     theme_id:number,
     img_path:string;
+    salary:number;
+    Answers: Answer[]
 }
+export type IdQuest = Quest['id']
+
+export type IdAnswer = Answer['id']
 
 export type StateQuestions = {
     questions: Quest[]
