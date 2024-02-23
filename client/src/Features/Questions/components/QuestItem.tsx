faa4c402cf89865991e174588b6b58536f89ae23
AlexandrBL <bbelozyrov.aaleksandr@gmail.com>
2024-02-23 12:12:48 +0300

    modals done

--- client/src/Features/Questions/components/QuestItem.tsx
+++ client/src/Features/Questions/components/QuestItem.tsx
@@ -1,16 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import type { Quest } from '../type'
 import '../Questions.css'
 
 function QuestItem({question}:{question:Quest}):JSX.Element {
   const [modals, setModals] = useState(false)
-  const [disabled, setDisabled] = useState(false)
+  const [disabled, setDisabled] = useState('false')
 
+  const useLocals = ():void => {
+    setModals(false)
+    setDisabled('true')
+    localStorage.setItem(`${question.question_text}`, 'true')
+  }
 
+  useEffect(()=>{
+    const name = `${question.question_text}`
+
+    if(!localStorage.getItem(name)) {
+      localStorage.setItem(`${question.question_text}`, 'false')
+    }
+    
+    setDisabled(localStorage.getItem(name))    
+  },[])
 
   return (
     <>
-    <button type='button' className={disabled ? 'disabled' : ''} onClick={
+    <button type='button' className={disabled === 'true' ? 'disabled' : ''} onClick={
       ()=> setModals(true)
     } >{
         question.id
@@ -20,10 +34,7 @@ function QuestItem({question}:{question:Quest}):JSX.Element {
   modals && 
   <div className="background">
     <div className='modals'>
-    <button type='button' onClick={()=>{
-      setModals(false)
-      setDisabled(true)
-      }} >x</button>
+    <button type='button' onClick={useLocals} >x</button>
     <h2>{question.question_text}</h2>
   </div>
   </div>
